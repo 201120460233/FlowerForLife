7d7f11dc0d9c54f10f81c7767dbc6cf0cb584451
shuni <shun@qinliannet.com>
2018-05-18 21:47:55 +0800

    新建项目

---
+++ bet305/bet305/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bet305
+//
+//  Created by shuni on 2018/5/18.
+//  Copyright © 2018年 shuni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bet305/bet305/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bet305
+//
+//  Created by shuni on 2018/5/18.
+//  Copyright © 2018年 shuni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
