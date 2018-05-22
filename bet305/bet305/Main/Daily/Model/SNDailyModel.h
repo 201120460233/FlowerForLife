4a7a9c9988cc60718c888d11b2302cf7230c39c7
shuni <shun@qinliannet.com>
2018-05-22 17:54:29 +0800

    dailyModel

---
+++ bet305/bet305/Main/Daily/Model/SNDailyModel.h
@@ -0,0 +1,13 @@
+//
+//  SNDailyModel.h
+//  bet305
+//
+//  Created by shuni on 2018/5/22.
+//  Copyright © 2018年 shuni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SNDailyModel : NSObject
+
+@end
---
+++ bet305/bet305/Main/Daily/Model/SNDailyModel.m
@@ -0,0 +1,14 @@
+
+//
+//  SNDailyModel.m
+//  bet305
+//
+//  Created by shuni on 2018/5/22.
+//  Copyright © 2018年 shuni. All rights reserved.
+//
+
+#import "SNDailyModel.h"
+
+@implementation SNDailyModel
+
+@end
